99b487ea9314aa01e86bc5842dbe81f9e0f07a0c
KrutNA <krutna@pm.me>
2020-06-27 17:13:39 +0300

    Priority queue addition.
    Added and tested converting data from Vec<u8> to priority queue
    implemented by binary_heap from std.
    
    Removed unnecessary mod import

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,8 @@
+mod queue;
+mod types;
+
 fn main() {
+    let data = "Lorem ipsum dolor sit amet";
+    let heap = queue::generate_from_data(&Vec::from(data.as_bytes()));
+    println!("{:#?}", heap);
 }
---
+++ src/queue.rs
@@ -0,0 +1,23 @@
+use std::collections::BinaryHeap;
+use crate::types::node::*;
+use std::fs::File;
+use std::io::Read;
+use std::{
+    rc::Rc,
+    cell::RefCell,
+};
+
+pub fn generate_from_data(data: &Vec<u8>) -> BinaryHeap<Element> {
+    let mut heap = Vec::new();
+    for &byte in data.iter() {
+	if let Some(i) = (0..heap.len()).into_iter()
+	    .find(|&x| if let Element::Data(x) = heap[x] { x.data == byte } else { false }) {
+	    if let Some(Element::Data(x)) = heap.get_mut(i) {
+		x.inc();
+	    }
+	} else {
+	    heap.push(Element::Data(Data::new(byte)));
+	}
+    }
+    heap.into()
+}
